07fddd1caf0af417d886d052baf28b3e0181f301
Matt Gohr <gohr89@gmail.com>
2017-12-13 22:05:58 -0500

    move images to first postions

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,13 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  var characterSelect = false;
+  var defender;
 
+  //THIS SHOULD WORK EVERY TIME I CLICK AN IMAGE
+  // your player image click
+  $("img").on("click", function() {
+    console.log("imageclick");
+    //if first time
+    if (characterSelect === false){
 
+      var image = $(this);
+      image.remove();
+      $("#your-character-space").append(image);
+    }
 
-      // numner click
-      $(".choose-section").on("click", function(){
-        console.log("imageclick");
+    else {
+        //check to see if it's in enemies
 
-      });
+        //move to defender
+    }
 
+  });
+
+  // move to Enemies section
+  $("#choose-section").on("click", function() {
+    console.log("choose section click");
+
+    //grab all images
+    $("#choose-section").each(function() {
+      var image = $(this).children("img");
+      image.remove();
+      $("#enemies-section").append(image);
+    });
+
+    characterSelect = true;
+  });
 
 
 });
