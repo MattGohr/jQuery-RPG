a182e8da9f06eb47d36d42f23ea0282ac6b31ace
Matt Gohr <gohr89@gmail.com>
2017-12-15 20:06:48 -0500

    only add one defender

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  var characterSelect = false;
-  var defender;
+  var defender, defenderFull = false, characterSelect = false;
+  // var characterPowers
 
   // move to Enemies section
   function moveToEnemies() {
@@ -11,39 +11,75 @@ $(document).ready(function() {
       var image = $(this).children("div");
       image.remove();
       $("#enemies-section").append(image);
+
     });
 
     //toggle the characterSelect
     characterSelect = true;
   };
 
-
-  // your player image click
-  $(".images").on("click", function() {
-    console.log("imageclick");
-
-    //grabs the column that the p tags are wrapped in
-    var image = $(this);
-
-    //image parent
-    var imageParent = $(this).parent();
-
-    //if in choose section
-    if (image.parent("#choose-section")) {
-      image.remove();
-      $("#your-character-section").append(image);
-
-      moveToEnemies();
-    }
-
-
-  });
+  function EnableClickEvent() {
+    $(".images").on("click", imageClick);
+  }
 
   // move Enemy to defender section
-  $("#enemies-section").on("click", function() {
+  function moveToDefender() {
     var image = $(event.target).parent().parent();
     image.remove();
     $("#defender-section").append(image);
+  };
+
+  // your player image click
+  function imageClick() {
+    console.log("imageclick");
+    //grabs the column that the p tags are wrapped in
+    var image = $(this);
+
+    var imageParentID = image.parent().attr('id');
+
+    //if in choose section
+    if (imageParentID === "choose-section") {
+
+      //remove image
+      image.remove();
+
+      //move to section
+      $("#your-character-section").append(image);
+      moveToEnemies();
+    }
+
+    //if in enemies section
+    if (imageParentID === "enemies-section") {
+
+      if (defenderFull === true) {
+        //skip
+      }
+
+      //else move image to the defender section
+      else {
+        image.remove();
+        $("#defender-section").append(image);
+        defenderFull = true;
+      }
+
+
+    }
+    EnableClickEvent();
+  }
+
+  //enable click event after full run
+  EnableClickEvent();
+
+  //attack button click
+  $("#attack-button").on("click", function(){
+
+    //check to see if chosen character and defener have been seleted
+
+      //run some function that attacks
+
+      //
   });
+
+
 
 });
